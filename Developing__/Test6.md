cc656ebd574da83d7662d17c66965bfb3b7e0c15
bskwon0526 <bskwon0526@gmail.com>
2019-04-10 14:11:02 +0900

    2019-04-10

---
+++ Developing__/Test6.md
@@ -0,0 +1,57 @@
+### Test6
+
+cv2_6.py
+
+***
+
+#### Hough Line Transfrom
+
+[hough_opencv_document][https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html]
+
+`cv2.HoughLines` `cv2.HoughLinesp`두가지를 사용해볼 예정이다
+
+***
+
+#### 허프변환
+
+허프변환은 이미지에서 이미지에서 모양을 찾는 가장 유명한 방법이다.
+
+![](https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Hough_transform_diagram.svg/800px-Hough_transform_diagram.svg.png)
+
+이미지 출저 : 위키피디아
+
+`y = mx + c` 를 `r = x cos [] + y sin []`의 형태로 표현한다
+
+3개의 점이 있고, 핑크색 직선을 찾는데 각 점 ( x , y )에서 삼각함수를 이용해 []를 1~180 사이로 변화를 하면서 r을 구한다. 그러면 ( [] , r )로 구성된 2차원 배열을 구할 수 있다.
+
+![](https://opencv-python.readthedocs.io/en/latest/_images/image022.png)
+
+ 이렇게 해서 구한 2차원 배열을 다시 그래프로 만들면 위와 같이 사인파 그래프가 구현된다고 한다. 세 그래프가 만날 확률이 가장 높은 [] 가 60이고 r 이 80인 점을 구할 수 있다. 
+
+이와 같은 방법을 OpenCv에서 구현할 수 있다.
+
+***
+
+`cv2.HoughLines(image,rho,theta,threshold)  `
+
+HoughLines 함수의 사용 방법이다.
+
++ image : CannyEdge를 선 적용해야함
++ rho : r 범위 0 ~ 1 실수
++ theta : 0 ~ 180 사이의 각도
++ threshold : 만나는 점의 기준, 숫자가 적으면 많은 선 검출, 정확도 떨어짐. 숫자 많으면 정확도 상승
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/cv2_6.py
@@ -0,0 +1,119 @@
+import numpy as np
+import cv2
+
+FilePath = '../vid/road2.mp4'
+movie = cv2.VideoCapture(FilePath)
+
+point1 = np.float32([[521,462],[185,607],[787,462],[1167,607]])
+point2 = np.float32([[10,10],[10,900],[900,10],[900,900]])
+#--------------------------------------------------------------------------------#
+def CallBackFunction(event,x,y,flag,params):
+    if(event == cv2.EVENT_LBUTTONDOWN):
+        print("x 좌표: ",x, "y 좌표", y)    
+#--------------------------------------------------------------------------------#
+def drawCircle_vertices(ret):
+    cv2.circle(ret, (521,462), 10, (255,0,0),-1)
+    cv2.circle(ret, (185,647), 10, (0,255,0),-1)
+    cv2.circle(ret, (787,462), 10, (0,0,255),-1)
+    cv2.circle(ret, (1167,647), 10, (0,0,0),-1) 
+#--------------------------------------------------------------------------------#
+def check_Movie(mov):
+    if mov.isOpened() == False:
+        print( 'Cannot open this File' + (FilePath))
+        exit()
+    else :
+        print("video is on ready!")
+
+    return 0
+#--------------------------------------------------------------------------------#
+def location_vertices_mask(img,vertices):
+    mask = np.zeros_like(img)
+    cha = img.shape[2]
+    mask_color = (255,) * cha   
+    
+    cv2.fillPoly(mask,vertices,mask_color)
+    mask_img = cv2.bitwise_and(img,mask)
+    return mask_img
+#--------------------------------------------------------------------------------#
+def give_cropimg(img):
+    frame_y = frame.shape[0]
+    frame_x = frame.shape[1]
+    location_vertices = [(0, frame_y-50),(frame_x / 2, frame_y / 2 ),(frame_x, frame_y-50),]
+    return location_vertices_mask(frame,np.array([location_vertices],np.int32))
+#--------------------------------------------------------------------------------#
+def function_Mask(img,lower_arrray,upper_array):
+    mask_ = cv2.inRange(img,lower_arrray,upper_array)
+    img = cv2.bitwise_and(img,img,mask = mask_)
+    img = cv2.cvtColor(img,cv2.COLOR_HSV2BGR)
+    return cv2.GaussianBlur(img,(9,9),0)
+#--------------------------------------------------------------------------------#
+
+check_Movie(movie)
+
+lower_yellow =  np.array([20,100,100])
+upper_yellow =  np.array([30,255,255])
+lower_white = np.array([0, 0, 212])
+upper_white = np.array([130, 255, 255])
+
+
+
+while(True):
+    
+    ret, frame = movie.read()
+    crop_img = give_cropimg(frame) 
+
+    hsv = cv2.cvtColor(crop_img,cv2.COLOR_BGR2HSV)
+    Get = cv2.getPerspectiveTransform(point1, point2)
+
+    dst = cv2.warpPerspective(frame, Get, (1000,1000))
+    hsv2 = cv2.cvtColor(dst,cv2.COLOR_BGR2HSV)
+    grayframe = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+
+
+    img_yellow_blur = function_Mask(hsv,lower_yellow,upper_yellow)
+    
+    img_white_blur = function_Mask(hsv,lower_white,upper_white)
+
+    img_white_blur2 = function_Mask(hsv2,lower_white,upper_white)
+
+    drawCircle_vertices(ret)
+    frame_y = frame.shape[0]
+    frame_x = frame.shape[1]
+
+    canny = cv2.Canny(grayframe,70,140)
+
+    #get_edge1 = cv2.Canny(img_white_blur,cv2.COLOR_BG2GRAY)
+    #lines1 = cv2.HoughLines(get_edge1,np.pi/180,100)
+    '''
+    for i in xrange(len(lines1)):
+        for rho, theta in lines1[i]:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 1000*(-b))
+            y1 = int(y0 + 1000*(a))
+            x2 = int(x0 - 1000*(-b))
+            y2 = int(y0 - 1000*(a))
+
+            cv2.line(img_white_blur,(x1,y1),(x2,y2),(0,0,255),2)
+'''
+    #cv2.imshow('see->',dst)
+    #cv2.imshow('frame',frame)
+    #cv2.imshow('crop',crop_img)
+    #cv2.setMouseCallback('frame',CallBackFunction)
+    #cv2.imshow('yellow',img_yellow_blur)
+    
+    cv2.imshow('white',img_white_blur)
+    cv2.imshow('perspective_see',img_white_blur2)
+
+
+    if cv2.waitKey(10) & 0xff == ord('q'):
+        break
+
+movie.release()
+cv2.destroyAllWindows()
+
+
+
+
